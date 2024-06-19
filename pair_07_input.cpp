96cc9f0c86630fedc02f38b234f085fe10d6fb97
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-06-19 17:19:27 +0600

    Create pair_07_input.cpp

---
+++ pair_07_input.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    pair<int,int>v;
+    /*cin>>v.first;
+    cin>>v.second;*/
+    cin>>v.first>>v.second;
+    cout<<v.first<<" "<<v.second<<endl;
+}
+

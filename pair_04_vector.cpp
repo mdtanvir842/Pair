e5433524d3b0dcb55cd36f8e777b118ed90eb750
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-06-19 16:31:44 +0600

    Create pair_04_vector.cpp

---
+++ pair_04_vector.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<pair<int,int>>v;
+    v.push_back({10,12});
+    v.push_back({8,11});
+    v.push_back({11,9});
+    v.push_back({2,4});
+    v.push_back({5,9});
+    v.push_back({10,11});
+    v.push_back({12,45});
+    v.push_back({2,54});
+    v.push_back({45,1});
+    /*for(int i=0;i<v.size();i++){
+        cout<<v[i].first<<" "<<v[i].second<<endl;
+    }*/
+    for(auto u:v){
+        cout<<u.first<<" "<<u.second<<endl;
+    }
+}
+

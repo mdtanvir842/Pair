269aa5e8f5b70f4641f1d335c16325735ce91004
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-06-19 16:17:35 +0600

    Create pair_01_assign.cpp

---
+++ pair_01_assign.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    pair<int,int>p;
+    p.first=5;
+    p.second=10;
+    cout<<p.first<<endl;
+    cout<<p.second<<endl;
+
+
+    pair<int,int>q;
+    q=make_pair(2,3);
+    cout<<q.first<<endl;
+    cout<<q.second<<endl;
+
+
+    pair<int,int>r;
+    r={4,5};
+    cout<<r.first<<endl;
+    cout<<r.second<<endl;
+
+
+    pair<int,int>s={10,11};
+    cout<<s.first<<endl;
+    cout<<s.second<<endl;
+}
